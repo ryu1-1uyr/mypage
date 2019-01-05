1c821c220ea0cab38bb0860b558ee7769a1d589b
s17001 <s17001@std.it-college.ac.jp>
2019-01-05 20:04:05 +0900

    るーてぃんぐ綺麗にした

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import index from '@/components/index'
+import about from '@/components/about'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/hoge',
       name: 'index',
       component: index
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: about
     },
   ]
 })
