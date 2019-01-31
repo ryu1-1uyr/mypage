41129c6f7200338f5bd180d6b4ed4f4bb5051de1
s17001 <s17001@std.it-college.ac.jp>
2019-01-31 19:07:13 +0900

    ページの内容を充実させた

--- src/components/history.vue
+++ src/components/history.vue
@@ -4,13 +4,15 @@
     <h2>LTしたやつ</h2>
     <ul>
       <li v-for="LT in LTList">
-        {{LT}}
+        <a :href="LT.url">{{LT.titel}}</a>
       </li>
     </ul>
+    <p>スライドだいたいここにあります</p>
+    <iframe src="https://www.slideshare.net/iharyuuicih/slideshelf" width="615px" height="470px" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:none;" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
     <h2>作ったもの</h2>
     <ul>
       <li v-for="making in Makinglist">
-        {{making}}
+        <a :href="making.url">{{making.titel}}</a>
       </li>
     </ul>
   </div>
@@ -21,8 +23,16 @@
       name: "sonota",
       data () {
           return {
-            LTList : [1,2,3],
-            Makinglist : [1,2,3]
+            LTList : [
+              { url:"https://re-build.connpass.com/event/114928/",titel:"沖縄 ×宮崎 × 秋田 × 福岡 × 山形 × 大阪 JSやらNight"},
+              { url:"https://ginowan.connpass.com/event/114092/",titel:"沖縄・宜野湾エンジニア勉強会 #6 in ギークハウス沖縄"},
+              { url:"https://codebase.connpass.com/event/83345/",titel:"エンジニア脳動会 -LTやろうぜ！NIGHT vol.1-"}
+      ],
+            Makinglist : [
+              { url:"https://s17001.github.io/vuePokeDex/dist/#/",titel:"ポケモン図鑑(Vue)"},
+              { url:"https://github.com/s17001/reactapp/tree/master/apiapp2",titel:"ポケモン図鑑(React)"},
+
+            ]
           }
     },
     }
